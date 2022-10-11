63f2b6887c29e60cdfec39d6bb2f35faf1d817ba
eyupGuc <meydanguresi@gmail.com>
2022-10-11 20:40:07 +0300

    v

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.scss';
+import Navbar from './components/Navbar/Navbar';
 
 function App(){
   return(
     <div className='App'>
-      <h1>App</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, ex doloribus inventore sint numquam fugit explicabo recusandae maxime eligendi totam earum possimus error sequi sunt iste veritatis atque nostrum? Est impedit sequi dolorem numquam.</p>
+      <Navbar/>
+      
     </div>
   )
 }
