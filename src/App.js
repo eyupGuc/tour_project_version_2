a7f32885fe0081cbb3e49d4b15b86da15108ac73
eyupGuc <meydanguresi@gmail.com>
2022-10-11 20:34:10 +0300

    v

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App(){
   return(
     <div className='App'>
       <h1>App</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, ex doloribus inventore sint numquam fugit explicabo recusandae maxime eligendi totam earum possimus error sequi sunt iste veritatis atque nostrum? Est impedit sequi dolorem numquam.</p>
     </div>
   )
 }
