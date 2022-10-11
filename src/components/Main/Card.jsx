2f8ff9b3e645d96fcecc0c64d37951d2d307c705
eyupGuc <meydanguresi@gmail.com>
2022-10-11 21:42:31 +0300

    v

--- src/components/Main/Card.jsx
+++ src/components/Main/Card.jsx
@@ -4,7 +4,13 @@ const Card=(data)=>{
     return(
 
         <div className="cards">
+            <div>
             <h1>{title}</h1>
+            <img src={image} alt="img" />
+            <div>
+                <p>{desc}</p>
+            </div>
+            </div>
         </div>
     )
 }
