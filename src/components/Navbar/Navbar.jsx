5ebe2af05f1209a9c82abdf65764340bc4f838c7
eyupGuc <meydanguresi@gmail.com>
2022-10-11 20:47:36 +0300

    v

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,14 @@ import './Navbar.scss';
 const Navbar=()=>{
     return(
         <div className='navbar'>
-<h1>Navbar</h1>
+   <div className="navbar">
+      <a href="#">About Us</a>
+      <a href="#">For You</a>
+      <a href="#">Services</a>
+      <a href="#">Blog</a>
+      <a href="#">Vlog</a>
+      <a href="#">Content</a>
+    </div>
         </div>
     )
 }
