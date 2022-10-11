66078580e8fe133ec209ecc837d9ebafa0083f94
eyupGuc <meydanguresi@gmail.com>
2022-10-11 21:37:37 +0300

    v

---
+++ src/components/Main/Card.jsx
@@ -0,0 +1,10 @@
+const Card=(data)=>{
+    console.log(data)
+    return(
+
+        <div className="cards">
+            <h1>Card</h1>
+        </div>
+    )
+}
+export default Card;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,17 +2,13 @@
 
 import './Main.scss';
 import {data} from '../../helper/data';
+import Card from './Card';
 const Main=()=>{
     console.log(data);
     return (
 
         <div className="card-container">
-            {data.map((item)=>{
-                return(
-<><h1>{item.title}</h1>
-<img src={item.image} alt="img" /></>
-                )
-            })}
+            {data.map((item)=><Card {...item}/>)}
             
         </div>
     )
